8f46ef901fa86d3a4600a96861478d17193c578e
ashlynns <ashlynn.steeves@icloud.com>
2019-12-13 15:56:05 -0700

    .

--- source/data_distribution.py
+++ source/data_distribution.py
@@ -1,6 +1,17 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 metadata = pd.read_csv('../Metadata.csv')
-sp_occ = pd.read_csv('../Species_Occurrence.csv')
+sp_occ = pd.read_csv('../Species_Occurrence.csv', index_col = 0)
 col_dict = {'Brucella sp':'#ebc04b', 'Brucella suis':'#f8a4d0','Brucella abortus': '#72a24a','Brucella canis': '#b8642f','Brucella ceti':'#59cf9e', 'Brucella melitensis':'#e8382e', 'Brucella neotomae':'#6c86e2', 'Brucella ovis':'#ba6eb4', 'Brucella pinnipedialis':'#f4792a' }
-strains = metadata['Strains'].unique()
+
+
+species = sp_occ.index.values 
+occurances = list(sp_occ['Species Occurrence'])
+
+bar_plot = plt.bar(species, occurances)
+
+for i, index in enumerate(species):
+	bar_plot[i].set_color(col_dict[index])
+	#bar_plot.text(occurances[i]+3, i+3, str(occurances[i]))
+plt.show()
